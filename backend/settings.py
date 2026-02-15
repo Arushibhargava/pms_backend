32b7e02e254363576a9288bff0eb40e3ee6fd152
Arushibhargava <arushibhargava2004@gmail.com>
2026-02-15 09:32:30 +0530

    Prepare for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,8 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 
 ALLOWED_HOSTS = ['*']
@@ -147,12 +148,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
